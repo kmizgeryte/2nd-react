cc0d09a5a73d294d412cbc3f0653eec4f6276c4a
kmizgeryte <mkristukas29@gmail.com>
2023-12-28 11:50:49 +0200

    ff

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Hero from './component/kitas'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Hero title="Hero" subtitle1="Info laba diena" subtitle="Info Hero" color="blue"/>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Kitas.jsx
@@ -0,0 +1,15 @@
+import './Kitas.css'
+
+
+const Hero =({title,subtitle1, subtitle, color}) => {
+    return(
+        <section className={`hero ${color}`}>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+            <p>{subtitle1}</p>
+
+        </section>
+    )
+}
+
+export default Hero
